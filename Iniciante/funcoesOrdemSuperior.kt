7064b897c95a1ed749eb6080bd627a7dc09eddba
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-20 23:59:35 -0300

    funcoes de ordem superior

---
+++ Iniciante/funcoesOrdemSuperior.kt
@@ -0,0 +1,16 @@
+// Funcao 
+fun main() {
+    val z:Int 
+
+    z = calculate(34, 90,::sum)
+   // z = calculate(34, 90){a,b -> a/b} divisão
+    println(z)
+}
+
+fun sum(a1:Int, a2:Int) = a1.plus(a2)
+
+// Função lambda
+fun calculate(n1:Int, n2:Int, operation:(Int, Int)->Int):Int {
+    val result = operation(n1, n2)
+    return result
+}
